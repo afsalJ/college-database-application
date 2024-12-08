c53dd30bf2564cb1bd9b624a2ede5ea96cb3d19a
afsalJ <mohamedafsal.jvf@gmail.com>
2024-12-08 17:43:43 +0530

    class/Student

---
+++ src/entity/Student.java
@@ -0,0 +1,6 @@
+package entity;
+
+public interface Student {
+    public void showDetails();
+    public void updateMarks(String marks);
+}
---
+++ src/entity/StudentImp.java
@@ -0,0 +1,65 @@
+package entity;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class StudentImp implements Student{
+    private String name;
+    private String course;
+    private Integer year;
+    private Double GPA;
+    private String marks;
+
+    public StudentImp(String name, String course, Integer year){
+        this.name = name;
+        this.course = course;
+        this.year = year;
+        this.GPA = 0d;
+        this.marks = "Math (0), Science(0), English (0)";
+    }
+
+    private void updateGPA(){
+        Pattern pattern = Pattern.compile("\\w+\\s*\\((\\d+)\\)");
+        Double totalScore = 0d;
+        Double totalSubjects = 0d;
+
+        for(String subjectMark: marks.split(",")){
+            Matcher matcher = pattern.matcher(subjectMark.trim());
+            if(matcher.find()){
+               Double mark = Double.parseDouble(matcher.group(1));
+               if(90<=mark){
+                totalScore = totalScore + 4.0;
+               } else if(80<=mark){
+                totalScore = totalScore + 3.7;
+               } else if(70<=mark){
+                totalScore = totalScore + 3.0;
+               } else if(60<=mark){
+                totalScore = totalScore + 2.0;
+               } else if(50<=mark){
+                totalScore = totalScore + 1.7;
+               }
+               totalSubjects++;
+            }else{
+                System.out.println("Marks are not valid");
+                return;
+            }
+        }
+
+        this.GPA = totalSubjects/totalScore;
+    }
+
+    public void showDetails(){
+        System.out.println(
+            "Student Name: "+this.name
+            +"\nCourse: "+this.course
+            +"\nYear: "+this.year
+            +"\nMarks: "+this.marks
+            +"\nGPA: "+this.GPA
+        );
+    }
+
+    public void updateMarks(String marks){
+        this.marks = marks;
+        updateGPA();
+    }
+}
